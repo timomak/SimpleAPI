2a2f44e74c1f94048d265ad4356d56745da7d458
timo <tmakhlay2@gmail.com>
2019-02-05 23:11:03 -0800

    Better server to be used for recaminder.

--- server.js
+++ server.js
@@ -15,12 +15,26 @@ const trainers = {
   }
 }
 
+const health = {
+  blood_pressure: 90.0,
+  heart_rate: 91.0,
+  drink_amount: 3.0,
+  workout_time: 5.0
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended: true}));
 
+app.post('/post', function(req, res){
+  console.log(req.body);      // your JSON
+  // console.log(res.json(req.body));
+  // res.json(req.body)
+  res.send(req.body);    // echo the result back
+
+});
 app.get('/', (req, res) => {
-  res.json(trainers)
+  res.json(health)
 });
 
 app.get('/trainer-api', (req, res) => {
@@ -42,6 +56,17 @@ app.get('/trainer-api/:arrayNum/age', (req, res) => {
 app.get('/trainer-api/:arrayNum/pokemon', (req, res) => {
   res.json(trainers[req.params.arrayNum].pokemon)
 });
+
+// // Update Add Trainer
+// app.put('/reviews/:id', (req, res) => {
+//   Review.findByIdAndUpdate(req.params.id, req.body)
+//     .then(review => {
+//       res.redirect(`/reviews/${review._id}`)
+//     })
+//     .catch(err => {
+//       console.log(err.message)
+//     })
+// })
 
 
 
